8b7bebe13ce9111e4382ed0a7164bcad3cca310e
Raul Felipe de Melo <melo.raulf@gmail.com>
2017-07-25 19:43:13 +0100

    added description to get root

--- src/routes/mailer.js
+++ src/routes/mailer.js
@@ -1,5 +1,21 @@
 const mailer = require('../services/mailer')
 
-module.exports = app =>{
+module.exports = app => {
+
     app.post('/mail', mailer)
+    app.get('/', (req, res) => {
+        res.status(404).json({
+            error: 'Get is not allowed. Try to access the following routes',
+            routes: {
+                "/mail": {
+                    method: "POST",
+                    params: {
+                        "from": "string",
+                        "subject": "string",
+                        "text": "string"
+                    }
+                }
+            }
+        })
+    })
 }
